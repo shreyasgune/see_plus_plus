7183774860e0e6de7ff284ae45178004e0f1d9e3
shreyasgune <sgune@protonmail.com>
2024-01-27 12:07:32 -0500

    file-templates - initial set of files

---
+++ build/CMakeFiles/starter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/starter.dir/main.cpp.o"
+  "CMakeFiles/starter.dir/main.cpp.o.d"
+  "starter"
+  "starter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/starter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
